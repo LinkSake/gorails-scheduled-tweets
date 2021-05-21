4fbe4cedbc13a562a8c2f7a067de30a77154aca6
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-05-21 17:11:14 -0600

    :construction: Part 11 - 13

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'main#index'
 
   get 'about', to: 'about#index'
+
+  get 'signup', to: 'registration#new'
+  post 'signup', to:'registration#create'
 end
