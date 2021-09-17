387d715c964e40a6d059ea6d9b3cfd2c705f2d8d
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-17 16:07:08 -0600

    🚧 Part 14-17

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'registration#new'
   post 'signup', to:'registration#create'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
 end
